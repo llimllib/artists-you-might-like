3a71a2b8eb65a7e2eead64761d41ade83b0f799f
bill <bill.mill@gmail.com>
2011-05-24 00:35:02 -0400

    deleting comments

--- utils/getsimilar.py
+++ utils/getsimilar.py
@@ -1,17 +1,5 @@
 import urllib
 
-#base_url = "http://ws.audioscrobbler.com/2.0/"
-#
-#base_url = "http://ws.audioscrobbler.com/2.0/?"
-#
-#params = {"method": "artist.getsimilar", "artist": "Local Natives", 'api_key':"f3826b9e6fb686fbca1f1277f9e74861"}
-#
-##this is the api way. might not be necessary?
-##f = urllib.urlopen(base_url + urllib.urlencode(params))
-##txt = f.read()
-#
-#bands = ["Local Natives", "Menomena", "Tame Impala"]
-
 def getsimilar(bands):
     #a dictionary of <band name>: (total score, # of appearances, bandname)
     similar = {}
