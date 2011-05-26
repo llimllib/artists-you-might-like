e7387935442c353f8d7214e76dfab604390d7ccb
bill <bill.mill@gmail.com>
2011-05-26 12:33:42 -0400

    sketching out use of a celery task to grab the bands

--- thepage/views.py
+++ thepage/views.py
@@ -3,7 +3,9 @@ from django.template import RequestContext
 from django.utils import simplejson
 from django.http import HttpResponse
 
-from artists_you_might_like.utils.getsimilar import getsimilar
+#from artists_you_might_like.utils.getsimilar import getsimilar
+
+from thepage.tasks import get_similar_bands
 
 def index(request):
     #I would use a generic view, but I don't want to deal with the 1.2/1.3 difference. So there.
@@ -11,5 +13,17 @@ def index(request):
 
 def similarity(request):
     bands = request.GET.getlist("b")
-    similar = getsimilar(bands)
-    return HttpResponse(simplejson.dumps(similar), mimetype='application/json')
+    #similar = getsimilar(bands)
+    task_id = get_similar_bands.delay(bands)
+    return HttpResponse(simplejson.dumps(task_id), mimetype='application/json')
+
+def check_result(request):
+    task_id = request.GET["task_id"]
+    task = get_similar_bands.AsyncResult(task_id)
+    if task.ready():
+        if task.state == SUCCESS:
+            return HttpResponse(simplejson.dumps(task.result, mimetype='application/json'))
+        else:
+            return HttpResponse(simplejson.dumps({"fail": task.result}), mimetype='application/json')
+    else:
+        return HttpResponse(simplejson.dumps("false"), mimetype='application/json')
