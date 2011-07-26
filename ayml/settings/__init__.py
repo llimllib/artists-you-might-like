1609913f8f6f7f8a586f5af15f693656988f4944
bill <bill.mill@gmail.com>
2011-07-25 23:27:11 -0400

    adding staticfiles to settings, not in local

--- ayml/settings/__init__.py
+++ ayml/settings/__init__.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'django.contrib.staticfiles',
     'djcelery',
     'thepage',
 )
