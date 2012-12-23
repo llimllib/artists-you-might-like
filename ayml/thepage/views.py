c746787a05189a96f18c7db3d08f2bfaf1d7b6ee
Bill Mill <bill.mill@gmail.com>
2012-12-23 00:05:31 -0500

    adding a results page

--- ayml/thepage/views.py
+++ ayml/thepage/views.py
@@ -9,6 +9,9 @@ def index(request):
     #I would use a generic view, but I don't want to deal with the 1.2/1.3 difference. So there.
     return render_to_response("index.html", context_instance=RequestContext(request))
 
+def results(request):
+    return render_to_response("results.html", context_instance=RequestContext(request))
+
 def similarity(request):
     bands = request.GET.getlist("b")
     #similar = getsimilar(bands)
